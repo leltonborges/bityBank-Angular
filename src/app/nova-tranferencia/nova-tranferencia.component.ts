3cadec866224e8693d6608e1400ea3574e47f115
Lelton Borges <leltondev@gmail.com>
2022-06-06 08:55:32 -0300

    feat: add bind properties

--- src/app/nova-tranferencia/nova-tranferencia.component.ts
+++ src/app/nova-tranferencia/nova-tranferencia.component.ts
@@ -6,15 +6,35 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./nova-tranferencia.component.scss']
 })
 export class NovaTranferenciaComponent implements OnInit {
+  private _valor: number;
+  private _destino: number;
 
   constructor() {
+  }
+
+  get valor(): number {
+    return this._valor;
+  }
+
+  set valor(value: number) {
+    this._valor = value;
+  }
+
+  get destino(): number {
+    return this._destino;
+  }
+
+  set destino(destino: number) {
+    this._destino = destino;
   }
 
   ngOnInit(): void {
   }
 
   transferir() {
-    console.log("Solicitado nova transferencia!");
+    console.log(`Solicitado nova transferencia!
+        valor: ${this.valor}
+        destino: ${this.destino}`);
   }
 
 }
