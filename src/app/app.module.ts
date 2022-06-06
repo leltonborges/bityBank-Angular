44ba00aa648d69e3f54953026b8d4a01a70aa0f2
Lelton Borges <leltondev@gmail.com>
2022-06-06 08:37:02 -0300

    feat: add event ngSubmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {NovaTranferenciaComponent} from "./nova-tranferencia/nova-tranferencia.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {NovaTranferenciaComponent} from "./nova-tranferencia/nova-tranferencia.c
     NovaTranferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
